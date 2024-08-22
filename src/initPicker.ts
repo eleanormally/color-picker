676de8c6864443bb08f09141ba4d2dfcf502da29
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-08-22 15:44:20 -0700

    formatted, added pnpm lock

--- src/initPicker.ts
+++ src/initPicker.ts
@@ -1,30 +1,36 @@
-import { Hct, redFromArgb, greenFromArgb, blueFromArgb } from "@material/material-color-utilities"
+import {
+  Hct,
+  redFromArgb,
+  greenFromArgb,
+  blueFromArgb,
+} from "@material/material-color-utilities"
 
 function hslToRgb(h: number, s: number, l: number) {
-  let r, g, b;
+  let r, g, b
 
   if (s === 0) {
-    r = g = b = l; // achromatic
+    r = g = b = l // achromatic
   } else {
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    r = hueToRgb(p, q, h + 1 / 3);
-    g = hueToRgb(p, q, h);
-    b = hueToRgb(p, q, h - 1 / 3);
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+    const p = 2 * l - q
+    r = hueToRgb(p, q, h + 1 / 3)
+    g = hueToRgb(p, q, h)
+    b = hueToRgb(p, q, h - 1 / 3)
   }
 
-  return [(r * 255), (g * 255), (b * 255)];
+  return [r * 255, g * 255, b * 255]
 }
 function hueToRgb(p: number, q: number, t: number) {
-  if (t < 0) t += 1;
-  if (t > 1) t -= 1;
-  if (t < 1 / 6) return p + (q - p) * 6 * t;
-  if (t < 1 / 2) return q;
-  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-  return p;
+  if (t < 0) t += 1
+  if (t > 1) t -= 1
+  if (t < 1 / 6) return p + (q - p) * 6 * t
+  if (t < 1 / 2) return q
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+  return p
 }
 export default function initPicker() {
-  const canvas = document.getElementById("picker-canvas") as HTMLCanvasElement || null
+  const canvas =
+    (document.getElementById("picker-canvas") as HTMLCanvasElement) || null
   if (canvas === null) {
     alert("no canvas found")
     return
@@ -36,7 +42,7 @@ export default function initPicker() {
   }
   const width = Number(canvas.width)
   const height = Number(canvas.height)
-  const ctx = canvas.getContext('2d')
+  const ctx = canvas.getContext("2d")
   if (ctx === null) {
     alert("unable to get canvas context")
     return
@@ -46,10 +52,10 @@ export default function initPicker() {
     let i = val / 4
     let y = Math.floor(i / width)
     let x = i % width
-    let dx = x / width * 2 - 1
-    let dy = y / height * 2 - 1
-    let radius = Math.sqrt((dx * dx) + (dy * dy))
-    let angle = Math.asin(dy / radius) / Math.PI * 180
+    let dx = (x / width) * 2 - 1
+    let dy = (y / height) * 2 - 1
+    let radius = Math.sqrt(dx * dx + dy * dy)
+    let angle = (Math.asin(dy / radius) / Math.PI) * 180
     if (dx < 0) {
       angle = 180 - angle
     }
@@ -70,13 +76,13 @@ export default function initPicker() {
     preview.style.left = (e.pageX - 8).toString() + "px"
     preview.style.top = (e.pageY - 8).toString() + "px"
   }
-  canvas.addEventListener("mousedown", function(e) {
+  canvas.addEventListener("mousedown", function (e) {
     canvas.style.cursor = "none"
     preview.style.visibility = "visible"
     doCursorMove(e as PointerEvent)
     canvas.addEventListener("pointermove", doCursorMove)
   })
-  canvas.addEventListener("mouseup", function() {
+  canvas.addEventListener("mouseup", function () {
     canvas.style.cursor = "default"
     canvas.removeEventListener("pointermove", doCursorMove)
   })
