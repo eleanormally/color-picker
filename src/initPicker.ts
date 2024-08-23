efb4f24f5e4891e02758816a314be62b04376171
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-08-22 18:45:52 -0700

    finalized aninest integration

--- src/initPicker.ts
+++ src/initPicker.ts
@@ -4,30 +4,26 @@ import {
   greenFromArgb,
   blueFromArgb,
 } from "@material/material-color-utilities"
-
-function hslToRgb(h: number, s: number, l: number) {
-  let r, g, b
-
-  if (s === 0) {
-    r = g = b = l // achromatic
-  } else {
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
-    const p = 2 * l - q
-    r = hueToRgb(p, q, h + 1 / 3)
-    g = hueToRgb(p, q, h)
-    b = hueToRgb(p, q, h - 1 / 3)
-  }
-
-  return [r * 255, g * 255, b * 255]
-}
-function hueToRgb(p: number, q: number, t: number) {
-  if (t < 0) t += 1
-  if (t > 1) t -= 1
-  if (t < 1 / 6) return p + (q - p) * 6 * t
-  if (t < 1 / 2) return q
-  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
-  return p
-}
+import {
+  addVec,
+  changeInterpFunction,
+  createAnimation,
+  getSlerp,
+  getStateTree,
+  mag,
+  modifyTo,
+  mulScalar,
+  newVec2,
+  normalize,
+  subVec,
+  Vec2,
+  ZERO_VEC2,
+} from "aninest"
+import {
+  getUpdateLayer,
+  localMomentumLayer,
+  restrictFromFunctionExtension,
+} from "@aninest/extensions"
 export default function initPicker() {
   const canvas =
     (document.getElementById("picker-canvas") as HTMLCanvasElement) || null
@@ -48,6 +44,7 @@ export default function initPicker() {
     return
   }
   let data = ctx.createImageData(width, height)
+
   for (let val = 0; val < data.data.length; val += 4) {
     let i = val / 4
     let y = Math.floor(i / width)
@@ -68,22 +65,68 @@ export default function initPicker() {
   ctx.putImageData(data, 0, 0)
   const preview = document.getElementById("selector") as HTMLElement
   function doCursorMove(e: PointerEvent) {
-    const rect = (e.target as HTMLElement).getBoundingClientRect()
+    const rect = canvas.getBoundingClientRect()
     const x = e.clientX - rect.left - 8
     const y = e.clientY - rect.top - 8
+    let v = { x, y }
+    if (mag(v) > width / 2) v = mulScalar(normalize(v), width / 2)
+    modifyTo(anim, { x, y })
+  }
+  type Anim = Vec2
+  const anim = createAnimation<Anim>(
+    newVec2(canvas.width / 2, canvas.height / 2),
+    getSlerp(0.05)
+  )
+  const updateLayer = getUpdateLayer<Anim>()
+  const momentumLayer = localMomentumLayer(0.5, canvas.width)
+  updateLayer.subscribe("update", anim => {
+    const { x, y } = getStateTree(anim)
     const { data: color } = ctx!.getImageData(x, y, 1, 1)
     preview.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-    preview.style.left = (e.pageX - 8).toString() + "px"
-    preview.style.top = (e.pageY - 8).toString() + "px"
-  }
-  canvas.addEventListener("mousedown", function (e) {
+    preview.style.transform = `translate(${x}px, ${y}px)`
+    // preview.style.left = x + "px"
+    // preview.style.top = y + "px"
+  })
+  const restrictExtension = restrictFromFunctionExtension<Anim>(state => {
+    const center = newVec2(canvas.width / 2, canvas.height / 2)
+    const fromCenter = subVec(state, center)
+    if (mag(fromCenter) > canvas.width / 2) {
+      const fromTopLeft = addVec(
+        mulScalar(normalize(fromCenter), width / 2 - Number.EPSILON * width),
+        center
+      )
+      const vel = momentumLayer.getVelocity()
+      if (vel == 0) {
+        modifyTo(anim, fromTopLeft)
+        return
+      }
+      const start = getStateTree(anim)
+      const end = fromTopLeft
+      const fromEnd = subVec(start, end)
+      const dist = mag(fromEnd)
+      changeInterpFunction(anim, getSlerp(dist / vel))
+      modifyTo(anim, fromTopLeft)
+    }
+  })
+  updateLayer.mount(anim)
+  momentumLayer.mount(anim)
+  restrictExtension(anim)
+  canvas.addEventListener("pointerdown", function (e) {
     canvas.style.cursor = "none"
     preview.style.visibility = "visible"
     doCursorMove(e as PointerEvent)
-    canvas.addEventListener("pointermove", doCursorMove)
-  })
-  canvas.addEventListener("mouseup", function () {
-    canvas.style.cursor = "default"
-    canvas.removeEventListener("pointermove", doCursorMove)
+    window.addEventListener("pointermove", doCursorMove)
+    setTimeout(() => {
+      momentumLayer.clearRecordedStates()
+    }, 50)
+    window.addEventListener(
+      "pointerup",
+      function () {
+        canvas.style.cursor = "default"
+        momentumLayer.startGlide()
+        window.removeEventListener("pointermove", doCursorMove)
+      },
+      { once: true }
+    )
   })
 }
